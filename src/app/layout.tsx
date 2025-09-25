cd2fa1603485261abcba0de65ded4b1a38d33c98
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-09-25 12:05:41 +0600

    assates and navber development

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import Navber from "@/components/shared/Navber";
 import Footer from "@/components/shared/Footer";
 
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navber />
-        <div className="h-screen">{children}</div>
+        <div className="h-screen">
+          <AntdRegistry>{children}</AntdRegistry>
+        </div>
         <Footer />
       </body>
     </html>
--- src/components/shared/Navber.tsx
+++ src/components/shared/Navber.tsx
@@ -1,10 +1,51 @@
-
+import { faShopify } from "@fortawesome/free-brands-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Search from "antd/es/input/Search";
+import Image from "next/image";
 const Navber = () => {
-    return (
-        <div className="text-2xl text-center">
-            <h1>Nabigation page</h1>
+  return (
+    <div>
+      <div className="container mx-auto py-4 px-2 m-3 ">
+        <div className="grid grid-cols-3 justify-center items-center">
+          <div className="mx-auto">
+            <Image
+              src={"/assates/logo-1.svg"}
+              width={100}
+              height={100}
+              alt="image logo"
+            />
+          </div>
+
+          <div>
+            <Search
+              placeholder="search your stor"
+              size="large"
+              enterButton
+              variant="outlined"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 mx-auto justify-center items-center">
+            <div className="flex gap-2">
+              <FontAwesomeIcon icon={faUser} className="w-8 hover:text-yellow-500 duration-300" />
+              <div>
+                <h1 className="hover:text-yellow-500 duration-300 font-semibold">ACCOUNT</h1>
+                <small className="hover:text-yellow-500 duration-300 font-semibold">Register | Login</small>
+              </div>
+            </div>
+
+            <div className="flex gap-5 mx-5">
+              <FontAwesomeIcon icon={faHeart} className="w-8 hover:text-yellow-500 duration-300" />
+              <FontAwesomeIcon icon={faShopify} className="w-8 hover:text-yellow-500 duration-300" />
+            </div>
+          </div>
         </div>
-    );
+
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navber;
