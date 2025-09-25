99c1d45c18fb134207ae9861b53f6888fbbdd3dc
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-09-25 09:00:01 +0600

    setup vegist website

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="text-2xl text-center">
+            <h1>This is footer page</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/shared/Navber.tsx
@@ -0,0 +1,10 @@
+
+const Navber = () => {
+    return (
+        <div className="text-2xl text-center">
+            <h1>Nabigation page</h1>
+        </div>
+    );
+};
+
+export default Navber;
