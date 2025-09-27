a7869a80e91da7e417a1cd5d68e181e24613c4be
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-09-27 20:05:59 +0600

    novber update some

--- src/components/shared/Navber.tsx
+++ src/components/shared/Navber.tsx
@@ -12,14 +12,15 @@ import Link from "next/link";
 import Submenu from "./Submenu";
 import SubCollection from "./SubCollection";
 const Navber = () => {
-  const menuDesign = "font-bold text-gray-500 mb-2 hover:text-yellow-500 duration-300 w-full";
+  const menuDesign =
+    "font-bold text-gray-500 mb-2 hover:text-yellow-500 duration-300 w-full";
   return (
     <div className="">
       <h1 className="text-center bg-gray-900 text-white p-4 font-semibold">
         <span className="animate-ping text-yellow-500 duration-300 opacity-100 font-semibold mx-2">
-           Free shipping
+          Free shipping
         </span>
-         orders from all item
+        orders from all item
       </h1>
       <div className="container mx-auto py-4 px-2 m-3 lg:px-30">
         <div className="grid lg:grid-cols-3 justify-center items-center">
@@ -38,7 +39,6 @@ const Navber = () => {
               size="large"
               enterButton
               variant="outlined"
-              
               className=""
             />
           </div>
@@ -64,18 +64,24 @@ const Navber = () => {
             </div>
 
             <div className="flex gap-5 mx-5 justify-center items-center">
-              <Link href={"/"}>
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  className="w-8 hover:text-yellow-500 duration-300 text-gray-500"
-                />
-              </Link>
-              <Link href={"/"}>
-                <FontAwesomeIcon
-                  icon={faShopify}
-                  className="w-8 hover:text-yellow-500 duration-300 text-gray-500"
-                />
-              </Link>
+              <div className="relative">
+                <Link href={"/"}>
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="w-8 hover:text-yellow-500 duration-300 text-gray-500"
+                  />
+                </Link>
+    <span className="absolute bg-yellow-500 font-semibold w-4 h-4 right-0 top-0 rounded-full flex justify-center items-center text-white">0</span>
+              </div>
+              <div className="relative">
+                <Link href={"/"}>
+                  <FontAwesomeIcon
+                    icon={faShopify}
+                    className="w-8 hover:text-yellow-500 duration-300 text-gray-500"
+                  />
+                </Link>
+                 <span className="absolute bg-yellow-500 font-semibold w-4 h-4 right-0 top-0 rounded-full flex justify-center items-center text-white">0</span>
+              </div>
             </div>
           </div>
         </div>
@@ -102,7 +108,6 @@ const Navber = () => {
                 <li className={menuDesign}>use me</li>
               </Link>
               <Link href={"/"}>
-
                 <li className={menuDesign}>vegist</li>
               </Link>
             </ul>
@@ -119,7 +124,9 @@ const Navber = () => {
           <li className="group text-gray-500 flex gap-1 hover:text-yellow-500 duration-200 font-semibold relative duration-300">
             <Link href={"/"}>Collection</Link>
             <FontAwesomeIcon icon={faAngleDown} className="w-3" />
-            <div className="absolute mt-10 invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-300 "><SubCollection /></div>
+            <div className="absolute mt-10 invisible opacity-0 group-hover:visible group-hover:opacity-100 duration-300 ">
+              <SubCollection />
+            </div>
           </li>
           <li className="text-gray-500 hover:text-yellow-500 duration-200 font-semibold">
             <Link href={"/"}>Blogs</Link>
@@ -128,13 +135,29 @@ const Navber = () => {
             <Link href={"/"}>Pages</Link>
             <FontAwesomeIcon icon={faAngleDown} className="w-3" />
             <ul className="absolute m-2 mt-10 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300 w-[150px] rounded-b-lg z-1 bg-white text-center">
-             <Link href={'/'}> <li className={menuDesign}>About US</li></Link>
-              <Link href={'/'}><li className={menuDesign}>Contact Us</li></Link>
-              <Link href={'/'}><li className={menuDesign}>Payment Policy</li></Link>
-              <Link href={'/'}><li className={menuDesign}>Privet Policy</li></Link>
-              <Link href={'/'}><li className={menuDesign}>Privet Policy</li></Link>
-              <Link href={'/'}><li className={menuDesign}>Return Policy</li></Link>
-             <Link href={'/'}> <li className={menuDesign}>conditions</li></Link>
+              <Link href={"/"}>
+                {" "}
+                <li className={menuDesign}>About US</li>
+              </Link>
+              <Link href={"/"}>
+                <li className={menuDesign}>Contact Us</li>
+              </Link>
+              <Link href={"/"}>
+                <li className={menuDesign}>Payment Policy</li>
+              </Link>
+              <Link href={"/"}>
+                <li className={menuDesign}>Privet Policy</li>
+              </Link>
+              <Link href={"/"}>
+                <li className={menuDesign}>Privet Policy</li>
+              </Link>
+              <Link href={"/"}>
+                <li className={menuDesign}>Return Policy</li>
+              </Link>
+              <Link href={"/"}>
+                {" "}
+                <li className={menuDesign}>conditions</li>
+              </Link>
             </ul>
           </li>
           <li className="text-gray-500 hover:text-yellow-500 duration-200 font-semibold">
