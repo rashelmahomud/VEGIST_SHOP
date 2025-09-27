7a6637d14ae8197e58b6c2f4a03132d3942f5a50
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-09-27 21:08:22 +0600

    added subcollection ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Banner from "@/components/ui/Banner";
+import DiscountCard from "@/components/ui/DiscountCard";
 
 export default function Home() {
   return (
     <div>
-
       <Banner />
+
+      <div className="lg:px-30 mt-20">
+        <DiscountCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/DiscountCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+const DiscountCard = () => {
+  return (
+    <div className="flex gap-10 justify-center items-center">
+      <div className="relative overflow-hidden">
+        <Image
+          src={"/assates/banner-1.webp"}
+          width={620}
+          height={200}
+          alt="dicount image"
+          className="transition-transform duration-300 hover:scale-110"
+        />
+        <div className="absolute left-0 top-20 pl-8">
+          <h1 className="lg:text-4xl text-2xl font-semibold mb-4 text-gray-700">
+            Fresh fruit,vegetable on <br /> our product
+          </h1>
+          <button className="py-2 px-6 bg-yellow-400 rounded-full font-semibold text-white hover:outline hover:yellow-none hover:bg-white hover:text-yellow-500 duration-300">
+            Shop Now
+          </button>
+        </div>
+      </div>
+      <div className="relative overflow-hidden">
+        <Image
+          src={"/assates/banner-3.webp"}
+          width={620}
+          height={200}
+          alt="dicount image"
+          className="transition-transform duration-300 hover:scale-110"
+        />
+        <div className="absolute left-0 top-20 pl-8">
+          <h1 className="lg:text-4xl text-2xl font-semibold mb-4 text-gray-700">
+            Vegetable eggplant <br /> 100% fresh food
+          </h1>
+          <button className="py-2 px-6 bg-yellow-400 rounded-full font-semibold text-white hover:outline hover:yellow-none hover:bg-white hover:text-yellow-500 duration-300">
+            Shop Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiscountCard;
