c326d620018ebae0fc97eeda21d5268c8668c1a4
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-10-13 10:48:11 +0600

    add details update responsive ness

--- src/app/tranding/[id]/page.tsx
+++ src/app/tranding/[id]/page.tsx
@@ -114,7 +114,7 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
         </span>
       </div>
       <div className="lg:px-36 p-3 my-10">
-        <div className="flex flex-row gap-5">
+        <div className="lg:flex lg:flex-row gap-5">
           <div className="basis-2/2">
             {productDetails.map((productD) => (
               <div
@@ -125,7 +125,7 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
                   <div>
                     <Image
                       src={productD.img}
-                      width={400}
+                      width={380}
                       height={500}
                       alt="image"
                     />
@@ -274,7 +274,7 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
                       </button>
                     </div>
                   </div>
-                  <div className="flex my-5 gap-5">
+                  <div className="flex lg:justify-start justify-center my-5 gap-5">
                     <h1 className="text-xl font-semibold text-gray-600">
                       Quantity :
                     </h1>
@@ -291,11 +291,11 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
                       </button>
                     </p>
                   </div>
-                  <div className="flex gap-5 p-2">
+                  <div className="flex gap-5 p-2 lg:justify-start justify-center">
                     <button>
                       <Link
                         href={""}
-                        className="px-14 py-3 text-white text-semibold bg-yellow-500 rounded-full"
+                        className="lg:px-14 px-3 py-3 text-white text-semibold bg-yellow-500 rounded-full"
                       >
                         Add to cart
                       </Link>
@@ -303,62 +303,64 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
                     <button className="text-gray-600">
                       <Link
                         href={""}
-                        className="px-14 py-3 text-white text-semibold bg-gray-800 rounded-full"
+                        className="lg:px-14 px-3 py-3 text-white text-semibold bg-gray-800 rounded-full"
                       >
                         Buy it now
                       </Link>
                     </button>
                   </div>
-                  <div className="flex items-center my-3 gap-5">
+                  <div className="flex items-center lg:justify-start justify-center my-3 gap-5">
                     <FontAwesomeIcon icon={faHeart} />
                     <h3>Wishlist</h3>
                   </div>
                   <p className="border-b border-gray-200 my-3"></p>
-                  <div className="text-gray-700 mb-3">
-                    <p>SKU: 123456</p>
-                  </div>
-                  <div className="flex items-center gap-5 ">
-                    <p className="text-xl font-semibold text-gray-500 mb-2">
-                      Share :
-                    </p>
-                    <div className="flex gap-3 text-gray-500">
+                  <div className="flex flex-col lg:items-start items-center">
+                    <div className="text-gray-700 mb-3">
+                      <p>SKU: 123456</p>
+                    </div>
+                    <div className="flex items-center gap-5 ">
+                      <p className="text-xl font-semibold text-gray-500 mb-2">
+                        Share :
+                      </p>
+                      <div className="flex gap-3 text-gray-500">
+                        <FontAwesomeIcon
+                          icon={faFacebook}
+                          className="hover:text-yellow-500"
+                        />
+                        <FontAwesomeIcon
+                          icon={faX}
+                          className="hover:text-yellow-500"
+                        />
+                        <FontAwesomeIcon
+                          icon={faPinterest}
+                          className="hover:text-yellow-500"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-2">
                       <FontAwesomeIcon
-                        icon={faFacebook}
-                        className="hover:text-yellow-500"
+                        icon={faCcMastercard}
+                        className="w-10 py-1 px-2 rounded-md bg-white border border-gray-300 duration-300"
                       />
                       <FontAwesomeIcon
-                        icon={faX}
-                        className="hover:text-yellow-500"
+                        icon={faPaypal}
+                        className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
                       />
                       <FontAwesomeIcon
-                        icon={faPinterest}
-                        className="hover:text-yellow-500"
+                        icon={faCcVisa}
+                        className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
+                      />
+                      <FontAwesomeIcon
+                        icon={faDollar}
+                        className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
                       />
                     </div>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <FontAwesomeIcon
-                      icon={faCcMastercard}
-                      className="w-10 py-1 px-2 rounded-md bg-white border border-gray-300 duration-300"
-                    />
-                    <FontAwesomeIcon
-                      icon={faPaypal}
-                      className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
-                    />
-                    <FontAwesomeIcon
-                      icon={faCcVisa}
-                      className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
-                    />
-                    <FontAwesomeIcon
-                      icon={faDollar}
-                      className="w-10 px-2 py-1 rounded-md bg-white border border-gray-300 duration-300"
-                    />
                   </div>
                 </div>
               </div>
             ))}
           </div>
-          <div className="basis-1/3">
+          <div className="basis-1/3 lg:mt-0 mt-3">
             <div className="text-center bg-gray-50 p-8">
               <FontAwesomeIcon
                 icon={faTruck}
@@ -395,7 +397,7 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
         </div>
       </div>
       <div>
-        <div className="flex gap-10 justify-center mb-5">
+        <div className="lg:flex gap-10 justify-center mb-5">
           <button className="border rounded-full py-2 px-3 border-gray-400">
             <Link onClick={() => setActive("1")} href={""}>
               DESCRIPTION
@@ -572,32 +574,48 @@ const TrandingId = ({ params }: { params: Promise<{ id: string }> }) => {
                     <button>
                       <Link
                         href={""}
-                        className="py-3 px-5 text-white font-semibold bg-yellow-500 rounded-full"
+                        className="lg:py-3 py-1 lg:px-5 px-2 text-white font-semibold bg-yellow-500 rounded-full"
                       >
                         Write a review
                       </Link>
                     </button>
-                    <div></div>
                   </div>
-                
                 </div>
-                  <div className="flex flex-col justify-center items-center">
-                 <div>   <h1 className="text-2xl font-semibold text-gray-500">Write a review</h1>
+                {/* // hidden subject  */}
+                <div className="flex flex-col justify-center items-center">
+                  <div>
+                    <h1 className="text-2xl font-semibold text-gray-500">
+                      Write a review
+                    </h1>
                     <h4 className="text-center my-2 text-gray-500">Rating</h4>
                     <Rate />
-                    <h4 className="text-center my-3 text-gray-500">Review Title</h4></div>
-                 <div>
-                  <div className="w-full">
-                    <input type="text" placeholder="Give your review a little" className="w-96 py-2 px-4 border border-gray-300" />
-                    <h2 className="text-center text-gray-500 my-3">review</h2>
-                    <textarea className="border border-gray-300 w-full p-2" placeholder="write your comments here" />
+                    <h4 className="text-center my-3 text-gray-500">
+                      Review Title
+                    </h4>
                   </div>
-                  <div className="flex gap-10 justify-center mt-3">
-                    <button className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg"><Link href={''}>Cancel Review</Link></button>
-                    <button className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg"><Link href={''}>Submite Review</Link></button>
+                  <div>
+                    <div className="w-full">
+                      <input
+                        type="text"
+                        placeholder="Give your review a little"
+                        className="w-96 py-2 px-4 border border-gray-300"
+                      />
+                      <h2 className="text-center text-gray-500 my-3">review</h2>
+                      <textarea
+                        className="border border-gray-300 w-full p-2"
+                        placeholder="write your comments here"
+                      />
+                    </div>
+                    <div className="flex gap-10 justify-center mt-3">
+                      <button className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg">
+                        <Link href={""}>Cancel Review</Link>
+                      </button>
+                      <button className="bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg">
+                        <Link href={""}>Submite Review</Link>
+                      </button>
+                    </div>
                   </div>
-                 </div>
-                  </div>
+                </div>
               </div>
             )}
           </div>
