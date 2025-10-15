39b9948af42ef488616cc007476f992ff23cd3f7
rashelmahomud <rashelmahmudraj1998@gmail.com>
2025-10-16 04:39:37 +0600

    added code design

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -132,7 +132,7 @@ const ProductId = ({ params }: ProductDetailsType) => {
 
   return (
     <div className="max-w-5xl mx-auto p-6">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div>
           <p className="text-2xl font-semibold text-center my-2">
             {product.title}
@@ -160,7 +160,7 @@ const ProductId = ({ params }: ProductDetailsType) => {
               />
             )}
           </div>
-          <div>
+          <div className="flex flex-row gap-10">
             {product?.images?.map((img, index): any => {
               const realIndex = index + 0;
               const selected = index === active;
@@ -174,8 +174,8 @@ const ProductId = ({ params }: ProductDetailsType) => {
                   }`}
                 >
                   <Image
-                    width={300}
-                    height={300}
+                    width={100}
+                    height={100}
                     src={img}
                     alt={"thambile"}
                     className="w-24 h-24 object-cover block"
